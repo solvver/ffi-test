bc2bb86b519d150b180df185271b3742d43bda38
Manuel Santillan <manuel.santillan@solvver.com>
2015-07-03 17:27:28 +0200

    Added local mac os x compiled lib to avoid manual install of GNU SL for mac users

--- ffi-test.js
+++ ffi-test.js
@@ -2,12 +2,28 @@ var ref=require("ref")
 var ArrayType=require("ref-array")
 var ffi=require("ffi")
 
-var DoubleArray=ArrayType(ref.types.double)
-var libgsl=ffi.Library("libgsl", {"gsl_stats_mean":["double", [DoubleArray, "int", "int"]]})
 
-var d=new Date();
+var DoubleArray=ArrayType(ref.types.double)
+try{
+    console.log("...Trying to load libgsl from library path. GNU Scientific Library should be installed in your system...")
+    var libgsl=ffi.Library("libgsl", {"gsl_stats_mean":["double", [DoubleArray, "int", "int"]]})
+    console.log("...libgsl loaded")
+} catch (err){
+    console.log("...libgsl is not installed in your system, trying to load './libgsl.dylib', a MAC OS X compiled version...")
+    try{
+    var libgsl=ffi.Library("./libgsl.dylib", {"gsl_stats_mean":["double", [DoubleArray, "int", "int"]]})
+        console.log("...libgsl loaded")
+    } catch(err){
+        console.log("...No libgsl available in your system. Please download and install a suitable version from https://www.gnu.org/software/gsl/")
+        console.log("...Quiting with error code 1")
+        process.exit(1)
+    }
+
+}
+console.log("...Starting test...")
 var res=libgsl.gsl_stats_mean(new DoubleArray([3.2,3.2,3.2]), 1, 3);
-console.log(res)
-console.log("sync", new Date().getTime()- d.getTime())
+if(res==3.2) console.log("TEST SUCCESS. Calculated mean equals expected 3.2 value")
+else console.log("TEST FAIL. Mean should be 3.2 but is", res)
+
 
 
